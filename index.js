301e2a6c4e2572107806659efd058d381f353c0b
Rajesh Nayak <rajesh1402@yahoo.com>
2019-08-30 12:39:44 +0530

    Added updated app function

--- index.js
+++ index.js
@@ -13,6 +13,27 @@ let net;
 //   const result = await net.classify(imgEl);
 //   console.log(result);
 // }
+async function app() {
+  console.log("Loading mobilenet..");
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log("Sucessfully loaded model");
+
+  await setupWebcam();
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById("console").innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
+}
 
 async function setupWebcam() {
   return new Promise((resolve, reject) => {
